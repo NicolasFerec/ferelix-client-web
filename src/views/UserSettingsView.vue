3e269fda28cd3e5ac65578b32e9944180c7660af
Nicolas <nicolasferec@outlook.com>
2025-12-12 00:28:29 +0100

    fix: update translation keys for database maintenance in English and French; add autocomplete attribute for password input in UserSettingsView

--- src/views/UserSettingsView.vue
+++ src/views/UserSettingsView.vue
@@ -67,6 +67,7 @@
               v-model="formData.password"
               name="password"
               type="password"
+              autocomplete="new-password"
               class="appearance-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-white bg-gray-800 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               :placeholder="$t('settings.leaveEmpty')"
             />
